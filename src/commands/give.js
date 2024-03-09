f392347fd528fbc3daf10c969800d90d283bfe69
Vladislav <doomgar5989@mail.ru>
2024-03-09 14:02:24 +0300

    aga

---
+++ src/commands/give.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: `give`,
+	access: [],
+	active: true,
+	aliases: [`богатство`],
+	cooldown: 3600,
+	requires: [],
+	async execute(client, ctx, utils) {
+		if (ctx.channel.id !== 739044027) return
+
+		return {
+			text: `!addpoints ${ctx.sender.login} 10000`,
+			reply: false
+		}
+	}
+}

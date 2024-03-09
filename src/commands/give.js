62e201e1287d8b0fed2601b8480d380dace63995
Vladislav <doomgar5989@mail.ru>
2024-03-09 14:12:58 +0300

    aga

--- src/commands/give.js
+++ src/commands/give.js
@@ -6,7 +6,7 @@ module.exports = {
 	cooldown: 3600,
 	requires: [],
 	async execute(client, ctx, utils) {
-		if (ctx.channel.id !== `739044027`) return
+		if (ctx.channel.id !== `739044027` || ctx.channel.id !== `239373609`) return
 
 		return {
 			text: `!addpoints ${ctx.user.login} 10000`,

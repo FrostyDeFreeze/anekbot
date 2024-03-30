e67aa5bcb6e2e69c8c7caf4b801930ee760b82e1
Vladislav <doomgar5989@mail.ru>
2024-03-30 19:53:24 +0300

    aga

--- src/commands/rank.js
+++ src/commands/rank.js
@@ -11,13 +11,13 @@ module.exports = {
 		const balance = userData.coins
 		const currRank = userData.rank
 		const nextRank = currRank + 1
-		const nextRankCost = ranks[nextRank].cost
+		const nextRankCost = ranks[nextRank]?.cost
 
-		const getRankInfo = rank => `${rank} [${ranks[rank].name}]`
+		const getRankInfo = rank => `${rank} [${ranks[rank]?.name || `Максимальный ранг`}]`
 
 		switch (ctx.args[0]?.toLowerCase()) {
 			case `up`: {
-				if (currRank >= 10) {
+				if (currRank === 10 || !nextRankCost) {
 					return {
 						text: `Ты достиг последнего ${currRank} ранга. Лечись, бро`,
 						reply: true
@@ -47,14 +47,30 @@ module.exports = {
 			}
 
 			default: {
-				return {
-					text: `Твой текущий ранг: ${getRankInfo(currRank)} \u{2027} Следующий ранг: ${getRankInfo(
-						nextRank
-					)} \u{2027} Необходимо: ${nextRankCost} \u{2027} У тебя: ${balance.toFixed(1)} \u{2027} ${
+				let currRankInfo = `Твой текущий ранг: ${getRankInfo(currRank)}`
+				let nextRankInfo = nextRankCost ? `Следующий ранг: ${getRankInfo(nextRank)}` : ``
+				let rankInfoText = ``
+				let remainingBalance = ``
+
+				if (nextRankCost) {
+					remainingBalance =
 						(nextRankCost - balance).toFixed(1) > 0
 							? `Осталось накопить: ${(nextRankCost - balance).toFixed(1)}`
 							: `Ты можешь повысить свой ранг, используя команду "${bb.config.Bot.Prefix}rank up"!`
-					}`,
+					rankInfoText += `Необходимо: ${nextRankCost} \u{2027} У тебя: ${balance.toFixed(1)}`
+				}
+
+				const result = [currRankInfo, nextRankInfo, rankInfoText, remainingBalance]
+					.reduce((acc, curr) => {
+						if (curr) {
+							acc.push(curr)
+						}
+						return acc
+					}, [])
+					.join(` \u{2027} `)
+
+				return {
+					text: result,
 					reply: true
 				}
 			}

219c9b2b62ae7eb5a261ed2d4652f9821d813380
Vladislav <doomgar5989@mail.ru>
2024-06-09 19:11:45 +0300

    AGA

--- src/commands/check.js
+++ src/commands/check.js
@@ -2,13 +2,13 @@ module.exports = {
 	name: `check`,
 	access: [],
 	active: true,
-	aliases: [`gde`],
+	aliases: [],
 	cooldown: 3,
 	requires: [],
 	async execute(client, ctx, utils) {
 		const user = ctx.args[0] ? bb.utils.parseUser(ctx.args[0]) : ctx.user.login
 
-		if (user === ctx.user.login) {
+		if (user.toLowerCase() === ctx.user.login) {
 			return {
 				text: `Ты прямо здесь aga`,
 				reply: true,
@@ -17,24 +17,33 @@ module.exports = {
 			}
 		}
 
-		const search = bb.utils.coins.findLastSeen(user)
+		const search = bb.utils.coins.getUserByLogin(ctx.channel.login, user)
 
 		if (search === null) {
 			return {
-				text: `Ни разу не видел этого пользователя PoroSad`,
+				text: `Ни разу не видел этого пользователя здесь PoroSad`,
 				reply: true,
 				emoji: true,
 				action: true
 			}
 		}
 
-		const channelLogin = bb.utils.unping(search.channel)
-		const userLogin = bb.utils.unping(search.info.login)
-		const lastSeen = search.info.lastSeen
+		const lastSeen = search.lastSeen
+
+		if (!lastSeen) {
+			return {
+				text: `Пользователь был здесь, однако я забыл записать его время PoroSad`,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		const userLogin = bb.utils.unping(search.login)
 		const timeDiff = bb.utils.humanizer(Date.now() - new Date(lastSeen))
 
 		return {
-			text: `В последний раз ${userLogin} был(а) обнаружен(а) на канале ${channelLogin} ${timeDiff} назад \u{1F92D}`,
+			text: `В последний раз ${userLogin} был(а) обнаружен(а) здесь ${timeDiff} назад \u{1F92D}`,
 			reply: true,
 			emoji: true,
 			action: true
---
+++ src/commands/gde.js
@@ -0,0 +1,43 @@
+module.exports = {
+	name: `gde`,
+	access: [],
+	active: true,
+	aliases: [],
+	cooldown: 3,
+	requires: [],
+	async execute(client, ctx, utils) {
+		const user = ctx.args[0] ? bb.utils.parseUser(ctx.args[0]) : ctx.user.login
+
+		if (user === ctx.user.login) {
+			return {
+				text: `Ты прямо здесь aga`,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		const search = bb.utils.coins.findLastSeen(user)
+
+		if (search === null) {
+			return {
+				text: `Ни разу не видел этого пользователя PoroSad`,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		const channelLogin = bb.utils.unping(search.channel)
+		const userLogin = bb.utils.unping(search.info.login)
+		const lastSeen = search.info.lastSeen
+		const timeDiff = bb.utils.humanizer(Date.now() - new Date(lastSeen))
+
+		return {
+			text: `В последний раз ${userLogin} был(а) обнаружен(а) на канале ${channelLogin} ${timeDiff} назад \u{1F92D}`,
+			reply: true,
+			emoji: true,
+			action: true
+		}
+	}
+}

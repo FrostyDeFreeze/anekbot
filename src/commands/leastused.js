0b4444f81277f58d231db722241e06ff767edba3
Vladislav <doomgar5989@mail.ru>
2024-09-02 19:42:40 +0300

    emotes tracker

---
+++ src/commands/leastused.js
@@ -0,0 +1,56 @@
+const got = require(`got`)
+
+module.exports = {
+	name: `leastused`,
+	access: [],
+	active: true,
+	aliases: [`lu`],
+	cooldown: 3,
+	requires: [],
+	async execute(client, ctx, utils) {
+		const count = !isNaN(ctx.args[0]) ? parseInt(ctx.args[0], 10) : 5
+		const res = await got(`https://7tv.markzynk.com/c/${ctx.channel.login}`).json()
+
+		if (res.success === false) {
+			return {
+				text: res.message,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		if (count > 20) {
+			return {
+				text: `Максимальное количество: 20`,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		const stvUser = await bb.services.stv.getUser(ctx.channel.id)
+		const defaultEmoteSet = stvUser.data.userByConnection.connections.find(i => i.platform === `TWITCH`).emote_set_id
+		const setEmotes = stvUser.data.userByConnection.emote_sets.find(i => i.id === defaultEmoteSet).emotes
+
+		const validEmoteIds = new Set(setEmotes.map(emote => emote.id))
+		const filteredEmotes = res.emotes.filter(emote => validEmoteIds.has(emote.id))
+		const sortedEmotes = filteredEmotes.sort((a, b) => a.count - b.count)
+
+		const top = sortedEmotes
+			.slice(0, count)
+			.map((emote, index) => {
+				const name = emote.alias !== null ? emote.alias : emote.name
+				const count = emote.count.toLocaleString(`en-EN`)
+				return `${index + 1}. ${name} (${count})`
+			})
+			.join(` \u{2027} `)
+
+		return {
+			text: top,
+			reply: true,
+			emoji: true,
+			action: true
+		}
+	}
+}
---
+++ src/commands/stats.js
@@ -0,0 +1,63 @@
+const got = require(`got`)
+
+module.exports = {
+	name: `stats`,
+	access: [],
+	active: true,
+	aliases: [`стата`],
+	cooldown: 3,
+	requires: [],
+	async execute(client, ctx, utils) {
+		const emote = ctx.args[0]
+
+		if (!emote) {
+			return {
+				text: `(7TV) Эмоут не указан`,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		const res = await got(`https://7tv.markzynk.com/c/${ctx.channel.login}`).json()
+
+		if (res.success === false) {
+			return {
+				text: res.message,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		const stvUser = await bb.services.stv.getUser(ctx.channel.id)
+		const defaultEmoteSet = stvUser.data.userByConnection.connections.find(i => i.platform === `TWITCH`).emote_set_id
+		const setEmotes = stvUser.data.userByConnection.emote_sets.find(i => i.id === defaultEmoteSet).emotes
+
+		const validEmoteIds = new Set(setEmotes.map(emote => emote.id))
+		const filteredEmotes = res.emotes.filter(emote => validEmoteIds.has(emote.id))
+		const find = filteredEmotes.find(i => (i.alias || i.name) === emote)
+
+		if (!find) {
+			return {
+				text: `Эмоут ${emote} не найден \u{2027} Название чувствительно к регистру`,
+				reply: true,
+				emoji: true,
+				action: true
+			}
+		}
+
+		const name = find.alias !== null ? find.alias : find.name
+		const count = find.count.toLocaleString(`en-EN`)
+
+		const sortedEmotes = filteredEmotes.sort((a, b) => b.count - a.count)
+		const position = sortedEmotes.findIndex(i => i.id === find.id) + 1
+
+		return {
+			text: `Эмоут ${name} был использован ${count} раз(а) \u{2027} Позиция в топе: ${position}`,
+			reply: true,
+			emoji: true,
+			action: true
+		}
+	}
+}

49eabcf41bb66d32aec908ac2aea7e00499756c5
Vladislav <doomgar5989@mail.ru>
2024-11-24 14:03:31 +0300

    penis

--- src/commands/word.js
+++ src/commands/word.js
@@ -46,20 +46,20 @@ module.exports = {
 		}
 
 		let describerReward = true
-		let text = `Поздравляю! Ты отгадал(а) слово "${data[ctx.channel.id].word}" и получаешь 50 монет \u{2027} Благодаря тебе ${
+		let text = `Поздравляю! Ты отгадал(а) слово "${data[ctx.channel.id].word}" и получаешь 25 монет \u{2027} Благодаря тебе ${
 			data[ctx.channel.id].describer.login
-		} также получает 50 монет за красноречивое объяснение \u{1F913}`
+		} также получает 25 монет за красноречивое объяснение \u{1F913}`
 
 		if (data[ctx.channel.id].isFailedToWhisper) {
 			describerReward = false
-			text = `Поздравляю! Ты отгадал(а) слово "${data[ctx.channel.id].word}" и получаешь 50 монет \u{1F913}`
+			text = `Поздравляю! Ты отгадал(а) слово "${data[ctx.channel.id].word}" и получаешь 25 монет \u{1F913}`
 		}
 
 		if (guess.toLowerCase() === data[ctx.channel.id].word.toLowerCase()) {
 			ctx.send(text, true, true, true)
 
 			bb.utils.coins.addCoins(ctx.user.id, ctx.channel.id, 25)
-			describerReward ? bb.utils.coins.addCoins(data[ctx.channel.id].describer.id, ctx.channel.id, 50) : null
+			describerReward ? bb.utils.coins.addCoins(data[ctx.channel.id].describer.id, ctx.channel.id, 25) : null
 
 			clearTimeout(bb.misc.guessTimeout)
 			delete data[ctx.channel.id]

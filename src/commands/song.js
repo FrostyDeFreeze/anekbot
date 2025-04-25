0886c55982a8a0c241410f9fcdab64612b271784
cuvi <doomgar5989@mail.ru>
2025-04-25 11:39:57 +0300

    ok

--- src/commands/song.js
+++ src/commands/song.js
@@ -74,7 +74,7 @@ module.exports = {
 				? `${bb.utils.randArr(statuses)} \u{25B6}`
 				: `Последний сыгранный у ${user} (${bb.utils.humanizer(new Date() - new Date(data[0].date[`uts`] * 1000), {
 						largest: 2
-				  })} назад) \u{23EF}`
+					})} назад) \u{23EF}`
 
 			return {
 				text: `${status} ${data[0].artist.name} - ${data[0].name}${text ? ` | ${text}` : ``}`,
